2fb67c864e751cdb39fcea5d5636c248bc9afcb9
Uladzislau Mikhayevich <3xecvte@gmail.com>
2025-12-23 09:12:02 -0800

    docs: #10 (task entity)
    add JavaDoc for Task Entity and Status enum

--- src/main/java/com/exxecute/taskflow/model/entity/Task.java
+++ src/main/java/com/exxecute/taskflow/model/entity/Task.java
@@ -8,29 +8,54 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import java.time.LocalDateTime;
 
+/**
+ * Task entity.
+ * Saves in DataBase.
+ *
+ * @author Uladzislau Mikhayevich
+ */
 @Getter
 @Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "tasks")
 public class Task {
+    /** id */
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    /**
+     * Title of the task.
+     *
+     * @see maximum size is 255 sumbols.
+     */
     @Column(nullable = false, length = 255)
     private String title;
 
+    /**
+     * Text description of the task.
+     */
     @Column(columnDefinition = "TEXT")
     private String description;
 
+    /**
+     * Status of the task, defined by enum.
+     */
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Status status;
 
+    /**
+     * Date and Time of the task creation.
+     */
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    /**
+     * Method that handles at Object creating,
+     * uses for setting creating Date and Time.
+     */
     @PrePersist
     protected void onCreate() {
         this.createdAt = LocalDateTime.now();
--- src/main/java/com/exxecute/taskflow/model/enums/Status.java
+++ src/main/java/com/exxecute/taskflow/model/enums/Status.java
@@ -1,5 +1,14 @@
 package com.exxecute.taskflow.model.enums;
 
+/**
+ * Uses for Task status definition.:
+ * <ul>
+ *   <li>NEW — Created</li>
+ *   <li>IN_PROGRESS — In work</li>
+ *   <li>DONE — Closed Task</li>
+ * </ul>
+ * @author Uladzislau Mikhayevich
+ */
 public enum Status {
     NEW,
     IN_PROGRESS,
