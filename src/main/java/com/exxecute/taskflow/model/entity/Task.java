ae08dc68662e895f1ea6d0e267835d55e60de602
Uladzislau Mikhayevich <3xecvte@gmail.com>
2025-12-23 09:01:48 -0800

    feat: #10 (task entity)
    task field description
    add status enum

--- src/main/java/com/exxecute/taskflow/model/entity/Task.java
+++ src/main/java/com/exxecute/taskflow/model/entity/Task.java
@@ -1,4 +1,38 @@
 package com.exxecute.taskflow.model.entity;
 
+import com.exxecute.taskflow.model.enums.Status;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "tasks")
 public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 255)
+    private String title;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Status status;
+
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+    }
 }
---
+++ src/main/java/com/exxecute/taskflow/model/enums/Status.java
@@ -0,0 +1,7 @@
+package com.exxecute.taskflow.model.enums;
+
+public enum Status {
+    NEW,
+    IN_PROGRESS,
+    DONE,
+}
