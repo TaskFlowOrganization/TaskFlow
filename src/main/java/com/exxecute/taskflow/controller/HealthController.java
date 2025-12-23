dfad0a97f13b069925a2fdde9876190c6a1cde63
Skrill <ivan.samal@student.ehu.lt>
2025-12-23 18:50:59 +0300

    health controller + http response

--- src/main/java/com/exxecute/taskflow/controller/HealthController.java
+++ src/main/java/com/exxecute/taskflow/controller/HealthController.java
@@ -1,4 +1,19 @@
 package com.exxecute.taskflow.controller;
 
-public class HealthController {
-}
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+    public class HealthController {
+
+        @GetMapping("/")
+        public String home(HttpServletResponse response) {
+            int status = response.getStatus();
+            System.out.println("status: " + status);
+            return "OK";
+        }
+    }
