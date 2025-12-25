b6c8d4fee6ebcd9f7c4650afe016dbdc45e4bca2
Uladzislau Mikhayevich <3xecvte@gmail.com>
2025-12-25 11:02:44 -0800

    feat: #12 (task service)
    add task service interface, implementation

--- src/main/java/com/exxecute/taskflow/service/TaskService.java
+++ src/main/java/com/exxecute/taskflow/service/TaskService.java
@@ -1,4 +1,18 @@
 package com.exxecute.taskflow.service;
 
-public class TaskService {
+import com.exxecute.taskflow.model.entity.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    Task create(Task task);
+
+    Task getById(Long id);
+
+    List<Task> getAll();
+
+    Task update(Long id, Task task);
+
+    void delete(Long id);
 }
