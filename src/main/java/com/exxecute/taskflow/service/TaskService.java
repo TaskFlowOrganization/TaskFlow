8164fdc3af43000a4d8bcfcbbe551b53cc85202b
Uladzislau Mikhayevich <3xecvte@gmail.com>
2025-12-25 20:48:55 -0800

    feat: SCRUM-12 (task service)
    - fix name of task service implementation
    - add to task definition of id name

--- src/main/java/com/exxecute/taskflow/service/TaskService.java
+++ src/main/java/com/exxecute/taskflow/service/TaskService.java
@@ -1,6 +1,7 @@
 package com.exxecute.taskflow.service;
 
 import com.exxecute.taskflow.model.entity.Task;
+import org.springframework.data.crossstore.ChangeSetPersister;
 
 import java.util.List;
 
@@ -8,11 +9,11 @@ public interface TaskService {
 
     Task create(Task task);
 
-    Task getById(Long id);
+    Task getById(Long id) throws ChangeSetPersister.NotFoundException;
 
     List<Task> getAll();
 
-    Task update(Long id, Task task);
+    Task update(Long id, Task task) throws ChangeSetPersister.NotFoundException;
 
-    void delete(Long id);
+    void delete(Long id) throws ChangeSetPersister.NotFoundException;
 }
---
+++ src/main/java/com/exxecute/taskflow/service/TaskServiceImpl.java
@@ -0,0 +1,87 @@
+package com.exxecute.taskflow.service;
+
+import com.exxecute.taskflow.model.entity.Task;
+import com.exxecute.taskflow.repository.TaskRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Implementation of task service.
+ *
+ * @author Uladzislau Mikhayevich
+ */
+@Service
+public class TaskServiceImpl implements TaskService {
+    /**
+     * Task Repository.
+     */
+    private final TaskRepository taskRepository;
+
+    /**
+     * Constructor.
+     *
+     * @param taskRepository Uses Task Repository.
+     */
+    public TaskServiceImpl(final TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    /**
+     * Create new Task.
+     * @param task New Task to create.
+     * @return New created Task.
+     */
+    @Override
+    public Task create(final Task task) {
+        return taskRepository.save(task);
+    }
+
+    /**
+     * Get Task by ID.
+     * @param id Task ID.
+     * @return Task found by ID.
+     * @throws ChangeSetPersister.NotFoundException if not found a Task with this ID.
+     */
+    @Override
+    public Task getById(final Long id) throws ChangeSetPersister.NotFoundException {
+        return taskRepository.findById(id)
+                .orElseThrow(ChangeSetPersister.NotFoundException::new); /* TODO: custom exception not this */
+    }
+
+    /**
+     * Get All Tasks.
+     * @return List of all Tasks.
+     */
+    @Override
+    public List<Task> getAll() {
+        return taskRepository.findAll();
+    }
+
+    /**
+     * Put new data to created Task.
+     * @param id ID of the Task to edit.
+     * @param task Data to edit Task.
+     * @return New Task.
+     * @throws ChangeSetPersister.NotFoundException if not found a Task with this ID.
+     */
+    @Override
+    public Task update(final Long id, final Task task) throws ChangeSetPersister.NotFoundException {
+        Task existingTask = getById(id);
+        BeanUtils.copyProperties(task, existingTask, Task.ID_NAME); /* TODO: maybe create task mapper for this */
+        return taskRepository.save(existingTask);
+    }
+
+    /**
+     * Delete Task by ID.
+     * @param id ID of the Task to delete.
+     * @throws ChangeSetPersister.NotFoundException if not found a Task with this ID.
+     */
+    @Override
+    public void delete(final Long id) throws ChangeSetPersister.NotFoundException {
+        Task existingTask = getById(id);
+        taskRepository.delete(existingTask);
+    }
+}
