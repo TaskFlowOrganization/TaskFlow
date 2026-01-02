67fb1b6d49da8a13ee9e42c3e8cfcbf9cff395dd
Skrill <ivan.samal@student.ehu.lt>
2026-01-02 23:33:07 +0300

    TaskRepo base methods

---
+++ src/main/java/com/exxecute/taskflow/service/TaskRepository.java
@@ -0,0 +1,18 @@
+package com.exxecute.taskflow.service;
+
+import com.exxecute.taskflow.model.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findAll();
+
+    boolean existsById(Long id);
+
+    List<Task> findByTitle(String title);
+
+}
