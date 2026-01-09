e5601bfb72aa1af29c0302502633702be2b2337d
Uladzislau Mikhayevich <3xecvte@gmail.com>
2026-01-09 15:30:15 +0200

    feat: SCRUM-20 (user entity)
    create user entity

---
+++ src/main/java/com/exxecute/taskflow/model/entity/User.java
@@ -0,0 +1,51 @@
+package com.exxecute.taskflow.model.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+/**
+ * User entity.
+ * Saves in DataBase.
+ *
+ * @author Uladzislau Mikhayevich
+ */
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "users")
+public class User {
+    /**
+     * id
+     */
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    /**
+     * Username
+     */
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    /**
+     * User email.
+     */
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    /**
+     * User tasks.
+     */
+    @OneToMany(
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            orphanRemoval = true
+    )
+    private List<Task> tasks;
+}
