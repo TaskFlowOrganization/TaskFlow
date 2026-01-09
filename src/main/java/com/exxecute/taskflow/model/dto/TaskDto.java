9f5f03763e0aa6d0512aa5c7c5ad3173b75245d4
Uladzislau Mikhayevich <3xecvte@gmail.com>
2026-01-09 15:47:09 +0200

    feat: SCRUM 18 (validations dto)
    - create dto
    - add spring-boot-starter-validation

--- src/main/java/com/exxecute/taskflow/model/dto/TaskDto.java
+++ src/main/java/com/exxecute/taskflow/model/dto/TaskDto.java
@@ -1,4 +1,38 @@
 package com.exxecute.taskflow.model.dto;
 
+import com.exxecute.taskflow.model.enums.Status;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * DTO for creating Task.
+ * Used for incoming REST requests.
+ *
+ * @author Uladzislau Mikhayevich
+ */
+@Getter
+@Setter
 public class TaskDto {
+    /**
+     * Title of the task.
+     * Max 255 symbols.
+     */
+    @NotNull(message = "Title must not be null")
+    @Size(min = 1, max = 255, message = "Title length must be between 1 and 255 characters")
+    private String title;
+
+    /**
+     * Description of the task.
+     * Optional field.
+     */
+    @Size(max = 5000, message = "Description must be less than 5000 characters")
+    private String description;
+
+    /**
+     * Status of the task.
+     */
+    @NotNull(message = "Status must not be null")
+    private Status status;
 }
