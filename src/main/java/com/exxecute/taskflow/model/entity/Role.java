adb2eadfe8c05aa6f4e1743ecfddcf90900d4c67
Skrill <ivan.samal@student.ehu.lt>
2026-02-20 16:17:13 +0300

    annotation added

--- src/main/java/com/exxecute/taskflow/model/entity/Role.java
+++ src/main/java/com/exxecute/taskflow/model/entity/Role.java
@@ -1,14 +1,27 @@
 package com.exxecute.taskflow.model.entity;
 
 import com.exxecute.taskflow.model.enums.RoleType;
-import jakarta.persistence.*;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.EnumType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "Roles")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Role {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Enumerated(EnumType.STRING)
