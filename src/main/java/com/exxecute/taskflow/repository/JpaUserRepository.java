628460c35d29663218204d0f5bcbd49cde3fbac7
Skrill <ivan.samal@student.ehu.lt>
2026-02-25 17:33:22 +0300

    refactor(db): migrate from h2, to postgres

--- src/main/java/com/exxecute/taskflow/repository/JpaUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.exxecute.taskflow.repository;
-
-import com.exxecute.taskflow.model.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaUserRepository extends JpaRepository<User, Long> {
-}
