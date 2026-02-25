09633016301d74151febb0f23bfcda3e570395c2
Skrill <ivan.samal@student.ehu.lt>
2026-02-25 17:33:40 +0300

    refactor(db): migrate from h2, to postgres

--- src/main/java/com/exxecute/taskflow/repository/TaskRepository.java
+++ src/main/java/com/exxecute/taskflow/repository/TaskRepository.java
@@ -1,11 +1,14 @@
 package com.exxecute.taskflow.repository;
 
 import com.exxecute.taskflow.model.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface TaskRepository  {
+@Repository
+public interface TaskRepository {
 
     List<Task> findAll();
 
